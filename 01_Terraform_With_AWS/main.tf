afddbc7735126876498e2b317f167f5c92837371
Okan Hollander <okan.hollander@quality.nl>
2024-04-01 12:07:09 +0200

    initial commit

---
+++ 01_Terraform_With_AWS/main.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_user" "admin-user" {
+  name = "lucy"
+  tags = {
+    Description = "Technical Team Leader"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_iam_policy" "adminUser" {
+  name   = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
+  user       = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.adminUser.arn
+}
+
+resource "aws_s3_bucket" "finance" {
+  bucket = "finance-010424"
+  tags = {
+    Description = "Finance and Payroll"
+  }
+}
+
+resource "aws_s3_bucket_object" "finance-2024" {
+  content = "${path.cwd}/finance/finance-2024.doc"
+  key = "finance-2024.doc"
+  bucket = aws_s3_bucket.finance.id
+}
+
+data "aws_iam_group" "finance-data" {
+  group_name = "finance-analysts"
+}
+
+resource "aws_s3_bucket_policy" "finance-policy" {
+  bucket = aws_s3_bucket.finance.id
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.finance.id}",
+            "Principal": {
+                "AWS": [
+                    "${data.aws_iam_group.finance-data.arn}"
+                ]
+            }
+        }
+    ]
+}
+
+  EOF
+}
